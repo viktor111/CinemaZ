f6bc7ca5a869c38ca73b870df2340b51e5f70c53
viktor111 <swifrorlilko@gmail.com>
2021-02-19 07:49:12 +0200

    ADD: article service and model functionality

--- CinemaZ/Models/Article.cs
+++ CinemaZ/Models/Article.cs
@@ -8,5 +8,13 @@ namespace CinemaZ.Models
     public class Article
     {
         public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Text { get; set; }
+
+        public string Title { get; set; }
+
+        public int Views { get; set; }
     }
 }
--- CinemaZ/Service/SqlArticleService.cs
+++ CinemaZ/Service/SqlArticleService.cs
@@ -18,27 +18,44 @@ namespace CinemaZ.Service
 
         public Article CreateArticle(Article article)
         {
-            throw new NotImplementedException();
+            _dbContext.Article.Add(article);
+
+            _dbContext.SaveChanges();
+
+            return new Article();
         }
 
         public Article DeleteArticle(Article article)
         {
-            throw new NotImplementedException();
+            _dbContext.Remove(article);
+
+            _dbContext.SaveChanges();
+
+            return new Article();
         }
 
-        public Article EdditArticle(Article article)
+        public Article EdditArticle(Article articleModel)
         {
-            throw new NotImplementedException();
+            Article articleToEdit = _dbContext.Article.Where(a => a.Id == articleModel.Id).FirstOrDefault();
+
+            articleToEdit.Date = DateTime.Now;
+            articleToEdit.Text = articleModel.Text;
+            articleToEdit.Title = articleModel.Title;
+            articleToEdit.Views = articleModel.Views;
+
+            _dbContext.SaveChanges();
+
+            return articleToEdit;
         }
 
         public Article GetArticle(int id)
         {
-            throw new NotImplementedException();
+            return _dbContext.Article.Where(a => a.Id == id).FirstOrDefault();
         }
 
         public List<Article> ListArticles()
         {
-            throw new NotImplementedException();
+            return _dbContext.Article.OrderBy(a => a.Date).ToList();
         }
     }
 }
