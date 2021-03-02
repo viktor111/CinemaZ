5011adef13e5920174bbf27fdea113c419b9e45c
viktor111 <swifrorlilko@gmail.com>
2021-03-02 23:11:03 +0200

    ADD: Add Room controller

---
+++ CinemaZ.Test/Controllers/RoomControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CinemaZ.Controllers;
+using CinemaZ.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CinemaZ.Test.Controllers
+{
+    [TestClass]
+    public class RoomControllerTest : DbContextSqlLite
+    {
+        private readonly SqlRoomService _sqlRoomService;
+        private readonly SqlCinemaService _sqlCinemaService;
+        private readonly RoomController _roomController;
+
+        public RoomControllerTest()
+        {
+            _sqlRoomService = new(_dbContext);
+            _sqlCinemaService = new(_dbContext);
+            _roomController = new(_sqlCinemaService, _sqlRoomService);
+        }
+
+        [TestMethod]
+        public void Room_ShouldReturnView()
+        {
+
+        }
+    }
+}
